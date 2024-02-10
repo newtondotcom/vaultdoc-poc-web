0fc3345d4665dcc37c046f015612f5d773d381fd
newtondotcom <asphalt9fr@gmail.com>
2024-02-11 00:59:22 +0100

    mostly styles

--- src/screens/Pdf.tsx
+++ src/screens/Pdf.tsx
@@ -1,7 +1,5 @@
 import { Worker } from '@react-pdf-viewer/core';
-// Import the styles
 import '@react-pdf-viewer/core/lib/styles/index.css';
-
 import ToolbarSlotsExample from '../utils/GenPdf';
 
 

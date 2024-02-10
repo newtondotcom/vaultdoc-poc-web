4777af765c0731d4431530ba63476916fd4ad06f
newtondotcom <asphalt9fr@gmail.com>
2024-02-10 23:53:32 +0100

    added router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Route, Router, RouterProvider, Routes, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import {FileExplorer} from './screens/FileExplorer.tsx';
+import {VFSMutable} from './screens/VFSMutable.tsx';
+import {Pdf} from './screens/Pdf.tsx';
+import './index.css';
+
+const router = createBrowserRouter([
+  { path: '/file-explorer', element: <FileExplorer /> },
+  { path: '/vfs-mutable', element: <VFSMutable /> },
+  { path: '/pdf', element: <Pdf/> },
+]
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);

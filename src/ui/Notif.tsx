e454ba3eeb693d7185c270f7175dc68801ba2110
newtondotcom <asphalt9fr@gmail.com>
2024-02-15 11:50:30 +0100

    toast doc link

--- src/ui/Notif.tsx
+++ src/ui/Notif.tsx
@@ -4,6 +4,9 @@ import 'react-toastify/dist/ReactToastify.css';
 // minified version is also included
 // import 'react-toastify/dist/ReactToastify.min.css';
 
+
+// DOC : https://fkhadra.github.io/react-toastify/update-toast
+
 export default function Notif(){
   const notify = () => toast("Wow so easy !");
   return (
